71023e89436e8665046881ad043401e577b131cd
vpasport <pasportnikov2000@gmail.com>
2024-09-07 18:29:10 +0300

    add ci

--- src/shared/constants/messages.ts
+++ src/shared/constants/messages.ts
@@ -33,7 +33,7 @@ export const startMessage: (chat: Chat.PrivateChat) => StartMessage = (
 					)
 				),
 				type: 'photo',
-				caption: `<b>Привет, ${
+				caption: `123 <b>Привет, ${
 					name ? `${name}, ` : ''
 				}вы попали на ...</b> \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
 				parse_mode: 'HTML',
